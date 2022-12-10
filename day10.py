4371d84c60221e56532e12ea5aa6f18df3dbf6cf
skjerns <14980558+skjerns@users.noreply.github.com>
2022-12-10 07:03:34 +0100

    improve code

--- day10.py
+++ day10.py
@@ -15,35 +15,37 @@ with open('day10_input.txt', 'r') as f:
 timer.start()
 commands = c.split('\n')[:-1]
 
+# pretend the register is a list, so we can easier keep track of what happened
 X = [1]
 
+# loop through commands
 for command in commands:
     match command.split(' '):
         case ['noop']:
-            X.append(X[-1])
+            X.append(X[-1])  # repeat previously register value
         case ['addx', n]:
-            X.append(X[-1])
-            X.append(X[-1] + int(n))
+            X.append(X[-1])  # repeat previously register value
+            X.append(X[-1] + int(n))  # only add in second cycle
 
 # the index has to be -1 as we already start with one value in the register
 signal_strength = [X[i-1]*i for i in range(20, len(X), 40)]
 print('\nsignal_strength is', sum(signal_strength), signal_strength)
+
+# prints: signal_strength is 14560 [420, 1260, 2100, 2380, 3780, 4620]
 
 #%% Part 1
 
 n_rows = 6
 n_cols = 40
 
-rows = []
-
 CRT = '\n'
-
-X_copy = X[::-1]
 
 for r in range(n_rows):
     for c in range(n_cols):
-        sprite_pos = X_copy.pop()
-        if abs(c-sprite_pos)<2:
+        sprite_pos = r*n_cols +c
+        sprite_val = X[sprite_pos]
+        # if sprite is overlapping with current cycle position: print char
+        if abs(c-sprite_val)<2:
             CRT += '█'  # use block char for better readability
         else:
             CRT += ' '
@@ -51,4 +53,13 @@ for r in range(n_rows):
     
     
 print(CRT)
+
+# this apparently prints
+# █  █ █   █ ██   ██
+# █ █  █   █ █ █ █
+# ██   █   █ ██   █ 
+# █ █  █   █ █     █
+# █ █  █   █ █     █
+# █  █ ███ █ █   ██
+# seriously. you don't believe me?
 timer.stop()
